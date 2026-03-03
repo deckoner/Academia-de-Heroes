20c6726236e96b22ec54f3d9d2a6b29af98d306f
deckoner <deckonerDev@gmail.com>
2026-03-03 19:12:40 +0100

    Esqueleto de los HTMLs

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from app.views import (
     HomeView,
     CrearPersonajeView,
@@ -8,9 +9,27 @@ from app.views import (
 )
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('personajes/crear/', CrearPersonajeView.as_view(), name='crear_personaje'),
-    path('personajes/', ListaPersonajesView.as_view(), name='lista_personajes'),
-    path('personajes/<int:personaje_id>/editar/', EditarPersonajeView.as_view(), name='editar_personaje'),
-    path('personajes/<int:personaje_id>/eliminar/', EliminarPersonajeView.as_view(), name='eliminar_personaje'),
+    path("", HomeView.as_view(), name="home"),
+    path("personajes/crear/", CrearPersonajeView.as_view(), name="crear_personaje"),
+    path("personajes/", ListaPersonajesView.as_view(), name="lista_personajes"),
+    path(
+        "personajes/<int:personaje_id>/editar/",
+        EditarPersonajeView.as_view(),
+        name="editar_personaje",
+    ),
+    path(
+        "personajes/<int:personaje_id>/eliminar/",
+        EliminarPersonajeView.as_view(),
+        name="eliminar_personaje",
+    ),
+    path(
+        "combate/",
+        TemplateView.as_view(template_name="combate/index.html"),
+        name="combate",
+    ),
+    path(
+        "estadisticas/",
+        TemplateView.as_view(template_name="estadisticas/index.html"),
+        name="estadisticas",
+    ),
 ]
