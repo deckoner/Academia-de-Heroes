58feee90d97ce8aa9d13b9bd6f174e02d1cac61f
deckoner <deckonerDev@gmail.com>
2026-03-10 15:52:22 +0100

    Bug en las urls arreglado

--- academia_de_heroes/settings.py
+++ academia_de_heroes/settings.py
@@ -73,79 +73,12 @@ WSGI_APPLICATION = "academia_de_heroes.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import os
-
-BASE_DIR = Path(__file__).resolve().parent
-
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
-
-SECRET_KEY = "django-insecure-key-for-development-only"
-
-
-# Application definition
-
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "app",
-]
-
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
-ROOT_URLCONF = "academia_de_heroes.urls"
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = "academia_de_heroes.wsgi.application"
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-if os.environ.get("DJANGO_TEST") == "1":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": ":memory:",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-            "TEST": {
-                "NAME": BASE_DIR / "test_db.sqlite3",
-            },
-        }
-    }
+}
 
 
 # Password validation
